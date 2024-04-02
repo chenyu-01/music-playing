f476a22343a5d07ccc58b3d535959f14d43cf883
chenyu-01 <chenyu101@outlook.com>
2024-04-02 09:53:29 +0800

    #DEPLOY add bun and production mode base url

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), VueDevTools()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production'
+  return {
+    base: isProduction ? '/music-playing/' : '/',
+    plugins: [vue(), VueDevTools()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-  base: import.meta.env.VITE_BASE_URL,
+  }
 })

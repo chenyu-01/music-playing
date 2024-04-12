3e7b4a21e1953e735ee300b46bb79fa1711fc70f
chenyu-01 <63953834+chenyu-01@users.noreply.github.com>
2024-04-12 08:33:46 +0000

    Deploying to gh-pages from @ chenyu-01/music-playing@93a7b665de04d93b52cecc048c2c8f98928b217b 🚀

---
+++ assets/AboutView-D7FFkskU.js
@@ -0,0 +1,1 @@
+import{_ as t,r as o,o as s,c as a,b as n,a as c}from"./index-D5CaGYME.js";const r={},_={class:"about"},i=c("h1",null,"This is an about page",-1);function u(d,l){const e=o("BaseTestButton");return s(),a("div",_,[i,n(e)])}const B=t(r,[["render",u]]);export{B as default};
